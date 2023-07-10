69982c755a372b243aa4aa40dd164b7235147690
uxmoon <uxmoon@gmail.com>
2023-07-10 17:14:54 -0300

    Add Nav component and styles for Layout

---
+++ components/Nav.js
@@ -0,0 +1,17 @@
+import styles from '../styles/Nav.module.css'
+const Nav = () => {
+  return (
+    <nav className={styles.nav}>
+      <ul>
+        <li>
+          <a href='#'>link</a>
+        </li>
+        <li>
+          <a href='#'>link</a>
+        </li>
+      </ul>
+    </nav>
+  )
+}
+
+export default Nav
