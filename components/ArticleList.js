218b1c38034b2a17a71af9773e38933f3038023f
uxmoon <uxmoon@gmail.com>
2023-07-11 10:23:17 -0300

    Add Article item component

--- components/ArticleList.js
+++ components/ArticleList.js
@@ -1,10 +1,11 @@
+import ArticleItem from './ArticleItem'
 import articleStyles from '../styles/Article.module.css'
 
 const ArticleList = ({ articles }) => {
   return (
     <div className={articleStyles.grid}>
       {articles.map((article) => (
-        <h3 key={article.id}>{article.title}</h3>
+        <ArticleItem article={article} key={article.id} />
       ))}
     </div>
   )
