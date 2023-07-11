af61904ef09bb596f829a37cbb58c77d0f5f2f22
uxmoon <uxmoon@gmail.com>
2023-07-11 11:31:41 -0300

    Using API data for article id

--- pages/article/[id]/index.js
+++ pages/article/[id]/index.js
@@ -1,3 +1,4 @@
+import { server } from '../../../config'
 import Link from 'next/link'
 const article = ({ article }) => {
   return (
@@ -11,10 +12,32 @@ const article = ({ article }) => {
 }
 
 // export const getServerSideProps = async (context) => {
+// export const getStaticProps = async (context) => {
+//   const res = await fetch(
+//     `https://jsonplaceholder.typicode.com/posts/${context.params.id}`
+//   )
+//   const article = await res.json()
+//   return {
+//     props: {
+//       article,
+//     },
+//   }
+// }
+
+// export const getStaticPaths = async () => {
+//   const res = await fetch('https://jsonplaceholder.typicode.com/posts/')
+//   const articles = await res.json()
+//   const ids = articles.map((article) => article.id)
+//   const paths = ids.map((id) => ({ params: { id: id.toString() } }))
+//   return {
+//     // paths: {params: {id: '1', id: '2'}}
+//     paths,
+//     fallback: false,
+//   }
+// }
+
 export const getStaticProps = async (context) => {
-  const res = await fetch(
-    `https://jsonplaceholder.typicode.com/posts/${context.params.id}`
-  )
+  const res = await fetch(`${server}/api/articles/${context.params.id}`)
   const article = await res.json()
   return {
     props: {
@@ -24,12 +47,11 @@ export const getStaticProps = async (context) => {
 }
 
 export const getStaticPaths = async () => {
-  const res = await fetch('https://jsonplaceholder.typicode.com/posts/')
+  const res = await fetch(`${server}/api/articles/`)
   const articles = await res.json()
   const ids = articles.map((article) => article.id)
   const paths = ids.map((id) => ({ params: { id: id.toString() } }))
   return {
-    // paths: {params: {id: '1', id: '2'}}
     paths,
     fallback: false,
   }
